67f2daad9f3d9bd08ea9e8214e5a6d8d3069b852
Your Name <soleil.mear@hotmail.fr>
2023-06-12 17:11:34 +0200

    test4

--- js/test4.js
+++ js/test4.js
@@ -1,13 +1,25 @@
 //l'utilisateur doit deviner le chiffre 8 et on doit afficher un texte selon qu'il a trouvé ou pas
-let guess = 8
-let number1
-
-console.log("Exercice 4: Deviner un chiffre")
-while (number1 !== guess){
-    //number1 = prompt('Entre un chiffre')*1
-    console.log(number1)
-    if(number1 !== guess){
-        console.log('Essaye encore')
-    }
-}
-console.log('Bravo!')
+window.addEventListener("load", function () {
+	let numberTest = document.querySelector("#guess-number1-input");
+	let guessResultSpan = document.querySelector(".guess-number1-result");
+console.log(numberTest)
+	function guessNumber() {
+        //debugger
+		let numberTestValue = numberTest.value;
+		numberTestValue = parseInt(numberTestValue);
+		let answer = 8;
+        guessResultSpan.innerText = ''
+		while (numberTestValue !== answer) {
+			console.log(numberTestValue);
+			console.log("hello?");
+			if (numberTestValue !== answer) {
+				guessResultSpan.innerText = "Raté, essaye encore";
+				console.log("Essaye encore");
+                return
+			}
+		}
+		guessResultSpan.innerText = "Bravo! La réponse était 8";
+		console.log("Bravo!");
+	}
+	numberTest.addEventListener("change", guessNumber);
+});
